3bcd7d8146e655a7b202b8da42bd8444ec0b1f7f
Samm Du <hmu@sammdu.com>
2021-11-27 18:42:06 -0500

    frontend layout and styling preliminarily complete

--- main/app.py
+++ main/app.py
@@ -4,11 +4,17 @@ Main application entry point. Serves the root ('/') endpoint.
 """
 
 from flask import Flask, render_template, request, make_response, jsonify
-from upstream.zendesk_common import API_URL_ROOT, AUTH_TUPLE
+from main.upstream.zendesk_common import API_URL_ROOT, AUTH_TUPLE
+from main.upstream.all_tickets import AllTickets
 
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    a = AllTickets(
+        api_url_root=API_URL_ROOT,
+        auth_tuple=AUTH_TUPLE,
+        page_size=25
+    )
+    return render_template('index.html', tickets=a.get_current_batch())
